5dd568db319ff3510934529706c4a0bc726fb018
Jon McCarthy <jtmcc93@gmail.com>
2019-01-04 19:14:14 -0700

    raw sql query for coffee tasting index

--- controllers/tastings_controller.js
+++ controllers/tastings_controller.js
@@ -28,14 +28,18 @@ module.exports = {
       });
   },
   index: (req, res) => {
-    knex("tastings as t")
-      .where("users_id", req.params.users_id)
-      .andWhere("coffee_id", req.params.coffee_id)
-      .orderBy("created_at", "desc")
-      // .join("tastings_tasting_notes as ttn", {
-      //   "ttn.tastings_id": "t.id"
-      // })
-      // .join("tasting_notes as tn", { "tn.id": "ttn.tasting_notes_id" })
+    knex
+      .raw(
+        `SELECT T.id, T.users_id, T.coffee_id, T.brew_method, T.body, T.acidity, T.sweetness, T.smoothness, T.rating, T.favorite, T.tasting_date, T.roasting_profile, T.description, T.created_at, T.updated_at, array_agg(TN.tasting_note) FROM TASTINGS AS T LEFT JOIN (SELECT TTN.id, TTN.tastings_id, TASTING_NOTES.tasting_note FROM TASTING_NOTES, TASTINGS_TASTING_NOTES AS TTN WHERE TASTING_NOTES.id = TTN.tasting_notes_id) AS TN ON T.id = TN.tastings_id WHERE T.users_id = ? AND T.coffee_id = ? GROUP BY T.id ORDER BY T.created_at DESC;`,
+        [req.params.users_id, req.params.coffee_id]
+      )
+      // knex("tastings as t")
+      //   .where("users_id", req.params.users_id)
+      //   .andWhere("coffee_id", req.params.coffee_id)
+      //   .join("tastings_tasting_notes as ttn", {
+      //     "ttn.tastings_id": "t.id"
+      //   })
+      //   .join("tasting_notes as tn", { "tn.id": "ttn.tasting_notes_id" })
       .then(results => {
         res.json(results);
       })
@@ -47,13 +51,13 @@ module.exports = {
   show: (req, res) => {
     knex("tastings as t")
       .where("id", req.params.tastings_id)
-      .then(tastings => {
+      .then(tasting => {
         // res.json(tastings);
         knex("tastings_tasting_notes as ttn")
           .join("tasting_notes", { "ttn.tasting_notes_id": "tasting_notes.id" })
-          .where("ttn.tastings_id", tastings[0].id)
+          .where("ttn.tastings_id", tasting[0].id)
           .then(notes => {
-            res.json({ tastings, notes });
+            res.json({ tasting, notes });
           });
       })
       .catch(err => {
